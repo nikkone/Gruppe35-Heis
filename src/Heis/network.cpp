2d1c41f29e9abe24fc0729375684d8ba073ca840
Abortkleshengeren <sales@scrapeitout.com>
2016-04-14 09:56:04 +0200

    stuff

--- src/Heis/network.cpp
+++ src/Heis/network.cpp
@@ -218,11 +218,14 @@ void network::udpBroadcaster(){
     udp::socket socket(io_service, udp::endpoint(udp::v4(), 0));
     socket.set_option(socket_base::broadcast(true));
     udp::endpoint broadcast_endpoint(address_v4::broadcast(), 8888);
-    char data[15];
+    string ip_s = ip.to_string();
+    char * data = new char[ip_s.size() + 1];
+    copy(ip_s.begin(), ip_s.end(), data);
+    data[ip_s.size()] = '\0';
     strcpy(data, (ip.to_string()).c_str());
     while(true){
         try{
-            socket.send_to(buffer(data), broadcast_endpoint);
+            socket.send_to(buffer(data, strlen(data)), broadcast_endpoint);
         } catch(...){cerr << "Could not connect to socket" << endl;}
         boost::this_thread::sleep(boost::posix_time::millisec(10000));
     }
